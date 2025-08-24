4a2bfde0483e9901d98b3da9d5c00fe1be3a41a9
GaelAlvs <gabriel.alvs19@gmail.com>
2025-08-24 14:07:15 -0300

    Primeiro Commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBar } from './components/menu-bar/menu-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MenuBar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('angular-psn-store');
